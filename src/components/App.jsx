91543345b4550e62e6455b37b62163d511253f34
Marcin Wrembel <marcin.wrembel@interia.pl>
2023-03-29 19:03:42 +0200

    initial

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <div>
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+      </Routes> */}
       React homework template
     </div>
   );
