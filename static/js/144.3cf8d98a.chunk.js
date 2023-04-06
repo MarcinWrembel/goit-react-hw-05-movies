449cb47118bbecc98c9c3d54542800af3a8a8a21
MarcinWrembel <110247627+MarcinWrembel@users.noreply.github.com>
2023-04-06 08:48:57 +0000

    Deploying to gh-pages from @ MarcinWrembel/goit-react-hw-05-movies@27dd186cd82d90c3c9326a7588fc0b3beba591db 🚀

---
+++ static/js/144.3cf8d98a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[144],{8144:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsxs)("main",{style:{textAlign:"center"},children:[(0,r.jsx)("b",{style:{fontSize:64},children:"404"}),(0,r.jsx)("p",{children:"Sorry, we couldn't find that page :("})]})}}}]);
+//# sourceMappingURL=144.3cf8d98a.chunk.js.map
